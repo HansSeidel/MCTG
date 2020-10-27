6c1aa3485f0f035e0138c4b6881c33e4e39967a2
Hans Seidel <if20b211@technikum-wien.at>
2020-10-27 18:20:00 +0100

    Wrote first get Request in MainClient. Need to adapt MyHttpCrud to new Request Style

--- src/main/java/MainClient.java
+++ src/main/java/MainClient.java
@@ -1,31 +1,59 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import bif3.swe.if20b211.api.MyHttpCRUD;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainClient {
     public static void main(String[] args) {
         System.out.println("start client");
 
-        try (Socket socket = new Socket("localhost", 8000);
+        /*
+         * Aim code:
+         * messager.write(to,message);
+         * inside messager:
+         *
+        connection.get("/messages/{id}");
+        connection.post("/message/10","MyMessage");
+        connection.put("message/1","MyNewMessage");
+        connection.delet("message/1","Deletede");
+
+        try {
+            MyHttpCRUD httpHandler = new MyHttpCRUD("localhost","https://postman-echo.com");
+            StringBuffer response = httpHandler.GET("get","foo1=bar1","foo2=bar2");
+            System.out.println(response.toString());
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        */
+
+
+        try (Socket socket = new Socket("stackoverflow.com", 80);
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
              BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
              SimpleBufferedWriter writer = new SimpleBufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
 
-            System.out.println("cli: " + reader.readLine());
-            System.out.println("cli: " + reader.readLine());
-            String input = null;
-            System.out.print("cli: ");
-            while (!"quit".equals(input = consoleReader.readLine())) {
-                writer.write(input);
-                System.out.print("cli: ");
-            }
-            writer.write(false, input);
+            String httpServer = "stackoverflow.com";
+            int serverPort = 80;
+            int timeoutMillis = 5000;
+            String httpRequest = "Get /questions/10673684/send-http-request-manually-via-socket HTTP/1.1";
+            String hostHeader = "Host: " + httpServer + "\r\n";
+
+            socket.setSoTimeout(timeoutMillis);
+            //writer.write(httpRequest,hostHeader,"");
+            writer.write(false,httpRequest,hostHeader);
+            writer.newLine();
+            writer.flush();
+
+            String str;
+            while ((str = reader.readLine()) != null)
+                System.out.println(str);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         System.out.println("close client");
 
     }
--- src/main/java/bif3/swe/if20b211/api/MyHttpCRUD.java
+++ src/main/java/bif3/swe/if20b211/api/MyHttpCRUD.java
@@ -1,24 +1,27 @@
 package bif3.swe.if20b211.api;
 
-import netscape.javascript.JSObject;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 
 public class MyHttpCRUD {
     //This Method should be clientSided and normally is build with JavaScript
     //Pseudocode
     private static String client;
     private static String auth_token;
-    private static URI server;
-    private JSObject jsObject;
+    private static String server;
 
     //private static <Type> metadata;
     //CREATE OBJECT BUILDER!!
 
-    MyHttpCRUD(String client, URI server){
-        this.client = client;
-        this.server = server;
+    public MyHttpCRUD(String client, String server){
+        MyHttpCRUD.client = client;
+        MyHttpCRUD.server = server;
     }
     public static String getAuth_token() {
         return auth_token;
@@ -28,19 +31,36 @@ public class MyHttpCRUD {
         MyHttpCRUD.auth_token = auth_token;
     }
 
-    public static URI getServer() {
+    public static String getServer() {
         return server;
     }
 
-    public static void setServer(URI server) {
+    public static void setServer(String server) {
         MyHttpCRUD.server = server;
     }
 
-    public static JSONObject GET(String path, String ... params){
+    public static StringBuffer GET(String path, String ... params) throws IOException {
+        String url = server + "/" +path + (params[0]!=null?"?":"");
+        for(String param : params)
+            url += param + (param.equals(params[params.length-1])?"":"&");
 
-        return null;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestMethod("GET");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(5000);
+
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        while ((line = reader.readLine()) != null)
+            responseContent.append(line);
+        reader.close();
+
+    return responseContent;
     }
-    public static JSONObject GET(URI otherUri, String path, String ... params){
+
+    public static JSONObject GET(URI ownUri, String path, String ... params){
 
         return null;
     }
